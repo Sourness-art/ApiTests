8626fb4830a605b638c7fb69c7b08386f8dbe829
DESKTOP-VF7PNFR\Victoria <kyrylova.viktoriia@nixsolutions.com>
2022-07-23 15:54:15 +0300

    first commit

---
+++ src/test/java/config/FootballApiConfig.java
@@ -0,0 +1,38 @@
+package config;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.BeforeClass;
+
+import static org.hamcrest.Matchers.lessThan;
+
+public class FootballApiConfig {
+
+    public static RequestSpecification football_requestSpec;
+    public static ResponseSpecification responseSpecification;
+
+    @BeforeClass
+    public static void setup() {
+        football_requestSpec = new RequestSpecBuilder()
+                .setBaseUri("http://api.football-data.org")
+                .setBasePath("/v4/")
+                .addHeader("X-Auth-Token", "fae8b099875d41f395c58dbb7f35556b")
+                .addHeader("X-Response-Control", "minified")
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
+                .build();
+
+        responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectResponseTime(lessThan(3000L))
+                .build();
+
+        RestAssured.requestSpecification = football_requestSpec;
+        RestAssured.responseSpecification = responseSpecification;
+    }
+}
